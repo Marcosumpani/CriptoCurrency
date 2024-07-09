0011e7d99bb5eeed3ebe6af973c37798649e4656
Marcosumpani <marcosumpani@gmail.com>
2024-07-09 07:49:50 -0300

    feat: add home page and some components

--- src/components/topBar/index.tsx
+++ src/components/topBar/index.tsx
@@ -3,7 +3,9 @@ import './topBar.css'
 function TopBar() {
     return (
         <div className="topBar">
-            <img src="/images/logo.png" alt="Logo" width="70px"/>       
+            <a href="/">
+                <img src="/images/logo.png" alt="Logo" width="70px" />
+            </a>
         </div>
     );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,16 @@
+import './home.css'
 import TopBar from "../../components/topBar"
 
 function Home() {
     return (<>
         <TopBar />
         <div className="home_container">
-            <h1>Bem-vindo ao CryptoCurrency!</h1>
-            <p>Acompanhe as cotações e as últimas notícias do mercado de criptomoedas em tempo real. Seja você um investidor experiente ou iniciante, estamos aqui para fornecer informações precisas e análises detalhadas para ajudá-lo a tomar decisões informadas.
-                Explore nosso site e aproveite ao máximo sua experiência no mundo das criptomoedas!</p>
-            <button>Confirir agora as cotações</button>
+            <div className='center_text'>
+                <h1>Bem-vindo ao CryptoCurrency!</h1>
+                <p>Acompanhe as cotações e as últimas notícias do mercado de criptomoedas em tempo real. Seja você um investidor experiente ou iniciante, estamos aqui para fornecer informações precisas e análises detalhadas para ajudá-lo a tomar decisões informadas.
+                    Explore nosso site e aproveite ao máximo sua experiência no mundo das criptomoedas!</p>
+                <button>Confirir agora as cotações</button>
+            </div>
         </div>
     </>
     )
