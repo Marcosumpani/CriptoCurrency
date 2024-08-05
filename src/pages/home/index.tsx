5abc3d21c834e609b5cd66750511ce16326996cc
Marcosumpani <marcosumpani@gmail.com>
2024-08-05 10:26:57 -0300

    feat: add home page complete

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,12 +4,15 @@ import TopBar from "../../components/topBar"
 function Home() {
     return (<>
         <TopBar />
+        <div className='backImage'>
+            <img  src="/images/background.png" alt="Background" />
+        </div>
         <div className="home_container">
             <div className='center_text'>
                 <h1>Bem-vindo ao CryptoCurrency!</h1>
                 <p>Acompanhe as cotações e as últimas notícias do mercado de criptomoedas em tempo real. Seja você um investidor experiente ou iniciante, estamos aqui para fornecer informações precisas e análises detalhadas para ajudá-lo a tomar decisões informadas.
                     Explore nosso site e aproveite ao máximo sua experiência no mundo das criptomoedas!</p>
-                <button>Confirir agora as cotações</button>
+                <a href="/list"><button>Conferir algumas cotações</button></a>
             </div>
         </div>
     </>
