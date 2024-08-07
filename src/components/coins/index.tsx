cfe150b99166b0bb2668137826d5531187a4b67d
menezesmateus <mateusgithub12@gmail.com>
2024-08-07 10:41:10 -0300

    feat: add images api

--- src/components/coins/index.tsx
+++ src/components/coins/index.tsx
@@ -1,10 +1,10 @@
 import './coins.css'
 
-function Coins({children}: any) {
+function Coins({image}: any) {
     return (
         <>
             <div className="coins">
-                <div>{children}</div>          
+                <div><img src={image} alt="" /></div>          
                 <div className="text-coin">
                     <h4 className="info">INFO</h4>
                     <h4 className="price">PREÇO</h4>
